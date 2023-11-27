a75542bf31bb25bc29d26fa2fc71ef3fe66f39d9
Jigin Jayaprakash <145117767+JiginJayaprakash@users.noreply.github.com>
2023-11-27 06:39:03 +0000

    added elastic docs changes

--- main/backend/rest_api/documents.py
+++ main/backend/rest_api/documents.py
@@ -11,11 +11,24 @@ PUBLISHER_INDEX.settings(
     number_of_replicas=1
 )
 
-
-
-
 @PUBLISHER_INDEX.doc_type
 class LogDataDocument(Document):
-
+    id = fields.IntegerField(attr='id')
+    user_id = fields.IntegerField(
+        fields={
+            'raw':{
+                'type': 'constant_keyword',
+            }
+            
+        }
+    )
+    timestamp = fields.DateField(
+        fields={
+            'date': {
+                'type': 'date',
+                
+            }
+        },
+    )
     class Django(object):
         model = Rest_Api
